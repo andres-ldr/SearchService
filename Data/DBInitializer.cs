97808654bd685acc456003410112dea0060ef46e
Andres <lopez.andresrigoberto@gmail.com>
2024-07-19 12:33:20 -0700

    add: mongo db initializer

---
+++ Data/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DBInitializer
+{
+    public static async Task InitDB(WebApplication app)
+    {
+        // Initialize the MongoDB connection
+        await DB.InitAsync("SearchService", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDBConnection")));
+
+        // Simple search server
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("Seeding database...");
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+    }
+}
