362f92d37e10e3746bf752c142fadd5c311efb11
Andres <lopez.andresrigoberto@gmail.com>
2024-07-19 13:13:32 -0700

    add: pagination

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -10,9 +10,9 @@ namespace SearchService;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<Item>> SearchItems(string searchTerm)
+    public async Task<ActionResult<Item>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
     {
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
 
         query.Sort(x => x.Ascending(a => a.Make));
 
@@ -21,8 +21,15 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber).PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
 
-        return Ok(result);
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
