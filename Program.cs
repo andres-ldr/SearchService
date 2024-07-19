a045c3953d1ca09b0c48d54f5065914381405263
Andres <lopez.andresrigoberto@gmail.com>
2024-07-19 12:33:05 -0700

    add: mongo db initialization

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DBInitializer.InitDB(app);
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e);
+}
+
 
 app.Run();
