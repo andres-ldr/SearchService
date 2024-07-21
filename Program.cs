7a28b3628024d75b0c3952721179b5dc28495b84
Andres <lopez.andresrigoberto@gmail.com>
2024-07-21 14:20:33 -0700

    add: MassTransit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 
+using MassTransit;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+// Make conncetion to RabbitMQ and configure endpoints
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 
 var app = builder.Build();
 
