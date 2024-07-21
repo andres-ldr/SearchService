4e716739eabc040b94cc4f9bf6283211bce320b3
Andres <lopez.andresrigoberto@gmail.com>
2024-07-20 21:11:54 -0700

    add: Policy to make resilient

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -10,15 +14,27 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-    await DBInitializer.InitDB(app);
-}
-catch (Exception e)
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
 
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DBInitializer.InitDB(app);
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine(e);
+    }
+});
+
 
 
 app.Run();
+
+// Make Resilint the sync call to the AuctionService
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError() // Handle the error
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3)); //Keep trying
