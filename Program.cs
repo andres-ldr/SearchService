f5654ed618fe9626aadcdb87456ab9b2b1a10ce5
Andres <lopez.andresrigoberto@gmail.com>
2024-07-21 21:22:33 -0700

    add: consumer

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+// Add AutoMapper to the project
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Make conncetion to RabbitMQ and configure endpoints
 builder.Services.AddMassTransit(x =>
 {
+    // Add consumers from the namespace containing AuctionCreatedConsumer
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    // Set the endpoint name formatter to kebab case
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
