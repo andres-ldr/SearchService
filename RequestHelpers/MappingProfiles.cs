c3b11a07bb876ea200ae92d47117d8827b9ea7f8
Andres <lopez.andresrigoberto@gmail.com>
2024-07-21 21:23:12 -0700

    add: profile mapper

---
+++ RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Contracts;
+
+namespace SearchService;
+
+/*
+    This class is used to map the properties of the AuctionCreatedConsumer to the Item class.
+*/
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<AuctionCreated, Item>();
+    }
+
+}
